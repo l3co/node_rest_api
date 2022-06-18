db118ca25362ddc9485bbb6b6879b2e727907671
leandro costa <lcosta.sp.br@gmail.com>
2022-06-18 19:14:21 -0300

    feat: :bug: crearte some routes for example

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const http = require("http")
 
 const port = 3000
 
+const rotas = {
+    "/": "Curso de Node",
+    "/sobre": "Info sobre o projeto",
+    "/livros": "Entrei na pag de livros",
+    "/autores": "Listagem de autores",
+    "/editora": "Pag de editora"
+}
+
 const server = http.createServer((req, resp) => {
     resp.writeHead(200, { "Content-Type": "text/plain" });
-    resp.end("Curso de Node");
+    resp.end(rotas[req.url]);
 
     console.log(req.headers);
 })
