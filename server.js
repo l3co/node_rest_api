a75adde99ffbea84e182ae1f0ef4e8b3f5c1fb07
leandro costa <lcosta.sp.br@gmail.com>
2022-06-19 22:29:35 -0300

    fix: :art: update serve.js, add express dependecies

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-const http = require("http")
+// const http = require("http")
 
-const port = 3000
+const port = process.env.PORT || 3000
 
-const rotas = {
-    "/": "Curso de Node",
-    "/sobre": "Info sobre o projeto",
-    "/livros": "Entrei na pag de livros",
-    "/autores": "Listagem de autores",
-    "/editora": "Pag de editora"
-}
+// const rotas = {
+//     "/": "Curso de Node",
+//     "/sobre": "Info sobre o projeto",
+//     "/livros": "Entrei na pag de livros",
+//     "/autores": "Listagem de autores",
+//     "/editora": "Pag de editora"
+// }
 
-const server = http.createServer((req, resp) => {
-    resp.writeHead(200, { "Content-Type": "text/plain" });
-    resp.end(rotas[req.url]);
+// const server = http.createServer((req, resp) => {
+//     resp.writeHead(200, { "Content-Type": "text/plain" });
+//     resp.end(rotas[req.url]);
 
-    console.log(req.headers);
-})
+//     console.log(req.headers);
+// })
 
-server.listen(port, () => {
+// server.listen(port, () => {
+//     console.log(`Servidor escuntando em http://localhost:${port}`);
+// })
+
+import app from "./src/app.js"
+
+app.listen(port, () => {
     console.log(`Servidor escuntando em http://localhost:${port}`);
 })
