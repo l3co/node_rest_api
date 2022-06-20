dde3d3d10448eb8cd4b600b42adf21d6e4b7953d
leandro costa <lcosta.sp.br@gmail.com>
2022-06-19 23:12:42 -0300

    feat: :art: finish crud

--- src/app.js
+++ src/app.js
@@ -27,11 +27,32 @@ app.get("/livros", (req, resp) => {
         .json(livros)
 })
 
+app.get("/livros/:id", (req, resp) => {
+    let index = buscarLivro(req.params.id)
+    resp.status(200).json(livros[index])
+})
+
 app.post("/livros", (req, resp) => {
     livros.push(req.body)
     resp
         .status(201)
         .send("Livro cadastrado com sucesso")
 })
+
+app.put("/livros/:id", (req, resp) => {
+    let index = buscarLivro(req.params.id)
+    livros[index].titulo = req.body.titulo
+    resp.json(livros[index])
+})
+
+app.delete("/livros/:id", (req, resp) => {
+    let index = buscarLivro(req.params.id)
+    livros.splice(index, 1)
+    resp.send(`livro ${index} removido com sucesso`)
+})
+
+function buscarLivro(id) {
+    return livros.findIndex(livro => livro.id == id)
+}
 
 export default app
