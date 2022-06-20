094c41b1c5138feaef97aefaf7da90b51f6a39ed
leandro costa <lcosta.sp.br@gmail.com>
2022-06-20 19:31:58 -0300

    feat(aula5): :art: populate editora

--- src/models/Livros.js
+++ src/models/Livros.js
@@ -5,7 +5,7 @@ const livroSchema = new mongoose.Schema(
         id: { type: String },
         titulo: { type: String, required: true },
         autor: { type: mongoose.Types.ObjectId, ref: "autores", required: true },
-        editora: { type: String, required: true },
+        editora: { type: mongoose.Types.ObjectId, ref: "editoras", required: true },
         paginas: { type: Number }
     }
 )
